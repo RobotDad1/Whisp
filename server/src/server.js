f5932f9855c01cbf3336866b6ef3a54b2ae4d8c2
RobotDad <renuverma78707@gmail.com>
2026-02-23 03:40:59 +0530

    update server.js for  setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,21 @@
 import express from 'express';
 // const cors = require('cors');
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
+
+const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
+
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+    });
+}
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
