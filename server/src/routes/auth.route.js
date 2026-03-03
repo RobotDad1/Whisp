756381de1f89e0c230f9419d453a68e802afe5c0
RobotDad <renuverma78707@gmail.com>
2026-03-03 18:24:01 +0530

    add cloudinary integration and enable user check route

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -13,6 +13,6 @@ router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
 
-// router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
+router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
 export default router;
