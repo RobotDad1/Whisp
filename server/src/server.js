92a9f52121717895f2e08c3773d620496d1d8d04
RobotDad <renuverma78707@gmail.com>
2026-03-07 17:38:43 +0530

    correcting refactor server to use socket server

--- server/src/server.js
+++ server/src/server.js
@@ -3,25 +3,21 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
-// import messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-
+import { app, server } from "./lib/socket.js";
 const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
-const app = express();
-app.use(express.json());
 
-
-// import { app, server } from "./lib/socket.js";
 
 app.use(express.json({ limit: "13mb" })); // req.body
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 
 if (ENV.NODE_ENV === "production") {
@@ -32,7 +28,7 @@ if (ENV.NODE_ENV === "production") {
     });
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server running on port: " + PORT);
     connectDB();
 });
